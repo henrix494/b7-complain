0093dafbef8b4302fd4f16bf2f2f0ef5312c600e
henrix49 <henrix494@gmail.com>
2023-06-09 00:33:52 +0300

    bug fix

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -3,6 +3,8 @@ import Post from "@/models/post";
 
 export const GET = async (request, res) => {
 	try {
+		res.setHeader("Cache-Control", "no-store");
+
 		await connectToDB();
 		const posts = await Post.find().sort({ timeStamp: -1 });
 		return new Response(JSON.stringify(posts), {
