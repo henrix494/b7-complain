67d8131a8ff6e481fd0c1c57ba6667afdda78370
henrix49 <henrix494@gmail.com>
2023-06-09 00:39:56 +0300

    bug fix

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,7 +1,9 @@
 import { connectToDB } from "@/utils/database";
 import Post from "@/models/post";
 
-export const GET = async (request, res) => {
+export const GET = async (req, res) => {
+	res.setHeader("Cache-Control", "no-store, must-revalidate");
+
 	try {
 		await connectToDB();
 		const posts = await Post.find().sort({ timeStamp: -1 });
