5e0817985e01d7b89fb261c6af89b599c8e5f343
henrix49 <henrix494@gmail.com>
2023-06-09 16:31:44 +0300

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		GOOGLE_ID: process.env.GOOGLE_ID,
-		GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-		MONGODB_URI: process.env.MONGODB_URI,
-		NEXTAUTH_URL: "https://b7-complain.vercel.app/",
-		NEXTAUTH_URL_INTERNAL: "https://b7-complain.vercel.app/",
-		NEXTAUTH_SECRET:
-			"sadjovnasoihcjn98123hcoinjviwejfc9812jdoinfv98uw3g912jdasincx9812",
-	},
 	experimental: { appDir: true },
 	webpack(config) {
 		config.experiments = { ...config.experiments, topLevelAwait: true };
