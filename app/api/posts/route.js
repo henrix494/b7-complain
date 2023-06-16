88f55ee3af5043544a320aaf7c87370e8ea7b7e7
henrix49 <henrix494@gmail.com>
2023-06-16 16:10:29 +0300

    added timeStamp

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,6 +1,8 @@
 import { connectToDB } from "@/utils/database";
 import Post from "@/models/post";
-
+export const dynamic = "force-dynamic";
+export const revalidate = 5;
+export const fetchCache = "force-no-store";
 export const GET = async (request) => {
 	try {
 		await connectToDB();
